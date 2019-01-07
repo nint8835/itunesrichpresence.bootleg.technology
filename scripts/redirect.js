cbed68721af3d90366895ca50d5812b4a1adcdcb
Riley Flynn <riley@rileyflynn.me>
2019-01-06 20:46:39 -0330

    Publishing to GH Pages

---
+++ scripts/redirect.js
@@ -0,0 +1,6 @@
+$(function() {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    if (w < 1200) {
+        window.location = "https://github.com/nint8835/iTunesRichPresence";
+    }
+});
